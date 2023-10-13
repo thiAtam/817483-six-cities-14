44f5a06e24fd4b5f30479b054ba3a964e7fa9455
gor <gorsayadyan1@gmail.com>
2023-10-14 00:10:34 +0300

    Отделил хэдер в отдельный компонент и вставил на главную страницу

--- src/pages/main-page.tsx
+++ src/pages/main-page.tsx
@@ -1,4 +1,5 @@
 import OneCardView from '../components/app/one-card-view';
+import HeaderPage from '../components/app/header';
 
 type MainPageProps = {
   countOffers: number;
@@ -7,35 +8,7 @@ type MainPageProps = {
 function toMainPage({countOffers}: MainPageProps): JSX.Element {
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <HeaderPage/>
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
@@ -78,7 +51,7 @@ function toMainPage({countOffers}: MainPageProps): JSX.Element {
           <div className="cities__places-container container">
             <section className="cities__places places">
               <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
+              <b className="places__found">{countOffers} places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
                 <span className="places__sorting-type" tabIndex={Number('0')}>

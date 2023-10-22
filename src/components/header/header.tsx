4a691bf2d68633a7da04142cdacd9665f8c3c9ff
gor <gorsayadyan1@gmail.com>
2023-10-22 11:06:33 +0300

    Внедрил компонент Logo, добавил на страницу login и компонент header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,12 @@
+import Logo from '../logo/logo';
+
 function Header(): JSX.Element {
   return (
     <header className="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
-            <a className="header__logo-link header__logo-link--active">
-              <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-            </a>
+            <Logo />
           </div>
           <nav className="header__nav">
             <ul className="header__nav-list">
